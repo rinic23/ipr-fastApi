be4888d120869b9bfa383b030a39e1b187ffe1fe
Erichev Egor <ErichevEA@lad24.ru>
2021-02-18 18:56:10 +0300

    Plug docker Compouse

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ RUN mkdir /app
 WORKDIR /app
 ADD requirements.txt /app
 RUN pip3 install -r requirements.txt
-EXPOSE 27689
+# EXPOSE 27689
 ADD . /app
 # CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "27689"]
--- core/db.py
+++ core/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 #ДЛЯ ЗАПУСКА ПРИЛОЖЕНИЯ В РУЧНУЮ
 # SQLALCHEMY_DATABASE_URL = "postgresql://egor2:egor2@localhost/newDataBase"
 #ДЛЯ ЗАПУСКА ПРИЛОЖЕНИЯ С DOCKER
-SQLALCHEMY_DATABASE_URL = "postgresql://egor2:egor2@0.0.0.0/newDataBase"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
