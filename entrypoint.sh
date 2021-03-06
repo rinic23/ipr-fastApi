d32616f2e1078fad33e8fbf6fa9289f896ee1bda
Erichev Egor <ErichevEA@lad24.ru>
2021-03-06 15:24:25 +0300

    add auto migration

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+alembic upgrade head
+alembic revision --autogenerate -m "New migration"
+uvicorn main:app --reload --host 0.0.0.0 --port 27689
--- user/models.py
+++ user/models.py
@@ -11,4 +11,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True, unique=True)
     name = Column(String)
     email = Column(String)
-    password = Column(DateTime)
+    password = Column(String)
